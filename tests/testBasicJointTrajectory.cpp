c5c0f45c84438885eeae8461f253e25215cae791
jgvictores <jcgvicto@gmail.com>
2018-12-13 00:51:42 +0000

    testBasicJointTrajectory better q than x

--- tests/testBasicJointTrajectory.cpp
+++ tests/testBasicJointTrajectory.cpp
@@ -33,22 +33,22 @@ TEST_F(BasicJointTrajectoryTest, BasicJointTrajectoryLine)
 {
     //-- Create line trajectory
     ASSERT_TRUE( iJointTrajectory->setDuration(20.0) );  // Must be high, or may be expanded due to defaults
-    std::vector<double> x(6);
-    x[0] = 0;  // x
-    x[1] = 0;  // y
-    x[2] = 0;  // z
-    x[3] = 0;  // o(x)
-    x[4] = 0;  // o(y)
-    x[5] = 0;  // o(z)
-    std::vector<double> xd(6);
-    xd[0] = 1;  // x
-    xd[1] = 0;  // y
-    xd[2] = 0;  // z
-    xd[3] = 0;  // o(x)
-    xd[4] = 0;  // o(y)
-    xd[5] = 0;  // o(z)
-    ASSERT_TRUE( iJointTrajectory->addWaypoint(x) );
-    ASSERT_TRUE( iJointTrajectory->addWaypoint(xd) );
+    std::vector<double> q(6);
+    q[0] = 0;
+    q[1] = 0;
+    q[2] = 0;
+    q[3] = 0;
+    q[4] = 0;
+    q[5] = 0;
+    std::vector<double> qd(6);
+    qd[0] = 1;
+    qd[1] = 0;
+    qd[2] = 0;
+    qd[3] = 0;
+    qd[4] = 0;
+    qd[5] = 0;
+    ASSERT_TRUE( iJointTrajectory->addWaypoint(q) );
+    ASSERT_TRUE( iJointTrajectory->addWaypoint(qd) );
     ASSERT_TRUE( iJointTrajectory->configurePath( IJointTrajectory::LINE ) );
     ASSERT_TRUE( iJointTrajectory->configureVelocityProfile( IJointTrajectory::TRAPEZOIDAL ) );
     ASSERT_TRUE( iJointTrajectory->create() );
